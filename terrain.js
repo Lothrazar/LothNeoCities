1084490c79b7a00f55679d2018916e40f3215e03
Sam Bassett <samson.bassett@gmail.com>
2013-10-05 16:38:53 -0700

    terrainjs

---
+++ terrain.js
@@ -0,0 +1,38 @@
+
+
+  
+Crafty.c(Tree.id, 
+{
+  init: function() 
+  {
+    this.requires('Actor, Color, Solid');
+    this.color(config.TREE_COLOUR);
+  },
+});
+ 
+Crafty.c(Wall.id, 
+{
+  init: function() 
+  {
+    this.requires('Actor, Solid, spr_wall_black'); 
+  },
+});
+
+
+
+Crafty.c(Water.id, 
+{
+  init: function() 
+  {
+    this.requires('Actor,  spr_water'); 
+  },
+});
+
+
+Crafty.c(Shallow.id, 
+{
+  init: function() 
+  {
+    this.requires('Actor,  spr_waterdk'); 
+  },
+});
