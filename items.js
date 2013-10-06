2f00381dd52d0dd02affe1bbbac3ef8f80019576
Sam Bassett <samson.bassett@gmail.com>
2013-10-06 15:26:41 -0700

    gun carry shoot test

--- items.js
+++ items.js
@@ -44,3 +44,73 @@ Crafty.c('Sword',
        
    }
 });
+
+
+Crafty.c('Gun',
+{
+    holder:null,
+   init:function()
+   {
+        this.requires('Actor');//TODO: holder and icon ?
+     
+    
+    }
+
+
+  ,shoot:function(dir)
+  {
+    if(this.ammo <= 0) 
+    { 
+         AUDIO.PLAY(AUDIO.reload);////left at default volume
+        return; 
+        
+     }//dont shoot if empty
+    
+    
+    
+    //origin is top left, so up and left are negative
+    
+     
+    if(dir===null)
+    {
+        //default direction to movement
+        
+        
+        var movingUp = (this._movement.y < 0 );//can both be false
+        var movingDown = (this._movement.y > 0 );
+        
+        var movingLeft = (this._movement.x < 0 );
+        var movingRight = (this._movement.x > 0 );
+        //valid directions are (n,s,e,w,ne,nw,se,sw)
+        
+        //first decide if N or W
+        if(movingUp) dir = 'n';
+        if(movingDown) dir = 's';
+        
+        //either dir is empty;  or it is n/s, so it gets added to the end
+        dir += (movingLeft) ? 'w' : '';
+        dir += (movingRight) ? 'e' : '';
+        
+        //we will never add both 'w' and 'e', add at most one of them or neither
+        
+        
+        //if player is stationary, dir will still be emtpy string at this point
+        if(dir == '') {return; }
+        
+    
+    }
+  //create it, then fire it in given direction
+    Crafty.e("Arrow").attr({x: this.x, y: this.y , w: config.ARROW_SIZE, h: config.ARROW_SIZE, z:50}).fired(dir);
+  
+    this.holder.updateAmmo(-1);//reduce ammo by one since this shot was successful
+ 
+   
+    AUDIO.PLAY(AUDIO.shoot,1);
+ 
+    setTimeout(function()
+    {
+        AUDIO.PLAY(AUDIO.reload);
+        
+    },180);
+  }
+});
--- player.js
+++ player.js
@@ -14,11 +14,10 @@ Crafty.c(Player.id,
   speed_water:Player.speed/6,
   
   weapon:null,
-  
+  gun:null,
   inventory:[],
   init: function() 
   { 
-      
     this.requires('Actor, Fourway, Color, Collision, Solid')
       .fourway(Player.speed)
       .color(Player.colour) 
@@ -39,7 +38,8 @@ Crafty.c(Player.id,
     });
     
     
-   
+      this.gun = Crafty.e('Gun').at(this.x,this.y);
+   this.gun.holder = this;//for updateammo reverse call
     
     this.bind('Moved',this.onMoved);
     
@@ -190,59 +190,7 @@ Crafty.c(Player.id,
   
   ,shoot:function(dir)
   {
-    if(this.ammo <= 0) 
-    { 
-         AUDIO.PLAY(AUDIO.reload);////left at default volume
-        return; 
-        
-     }//dont shoot if empty
-    
-    
-    
-    //origin is top left, so up and left are negative
-    
-    var movingUp = (this._movement.y < 0 );//can both be false
-    var movingDown = (this._movement.y > 0 );
-    
-    var movingLeft = (this._movement.x < 0 );
-    var movingRight = (this._movement.x > 0 );
-     
-    if(dir===null)
-    {
-        //default direction to movement
-        
-    
-        //valid directions are (n,s,e,w,ne,nw,se,sw)
-        
-        //first decide if N or W
-        if(movingUp) dir = 'n';
-        if(movingDown) dir = 's';
-        
-        //either dir is empty;  or it is n/s, so it gets added to the end
-        dir += (movingLeft) ? 'w' : '';
-        dir += (movingRight) ? 'e' : '';
-        
-        //we will never add both 'w' and 'e', add at most one of them or neither
-        
-        
-        //if player is stationary, dir will still be emtpy string at this point
-        if(dir == '') {return; }
-        
-    
-    }
-  //create it, then fire it in given direction
-    Crafty.e("Arrow").attr({x: this.x, y: this.y , w: config.ARROW_SIZE, h: config.ARROW_SIZE, z:50}).fired(dir);
-  
-    this.updateAmmo(-1);//reduce ammo by one since this shot was successful
- 
-   
-    AUDIO.PLAY(AUDIO.shoot,1);
- 
-    setTimeout(function()
-    {
-        AUDIO.PLAY(AUDIO.reload);
-        
-    },180);
+      this.gun.shoot(dir);
   }
 
 ,hitNPC:function(data)
